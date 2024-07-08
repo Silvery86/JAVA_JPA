7609adcfe744d29f3cd40520350026bd2b99e30d
giang8692@gmail.com <giang8692@gmail.com>
2024-07-08 19:14:07 +0700

    upload project
    (cherry picked from commit 083fcf6bbdd20359fa8e6c2d79dd6ebaf11cc647)

---
+++ src/main/java/com/t2208e/T2208E_Java_JPA/dto/EmployeeDto.java
@@ -0,0 +1,9 @@
+package com.t2208e.T2208E_Java_JPA.dto;
+
+import jakarta.persistence.Column;
+
+public class EmployeeDto {
+    private Long id;
+    private String name;
+    private String wage;
+}

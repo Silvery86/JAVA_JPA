559206fcf81778b8d8044620303640e2677e52b8
NguyenGiang8692 <109542138+Silvery86@users.noreply.github.com>
2024-08-02 20:38:49 +0700

    Revert "Lab2"

--- src/main/java/com/t2208e/T2208E_Java_JPA/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.t2208e.T2208E_Java_JPA.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
